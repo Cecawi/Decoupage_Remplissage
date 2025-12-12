2137e40f7cf17fb3867975f1b8ebe0ba2bbfcda9
Celine Ye <yeceline26@gmail.com>
2025-12-12 01:20:19 +0100

    Add unity manager for interactive polygon clipping
    Ajout d’un script FenetrageManager pour saisir à la souris un polygone et une fenêtre dans Unity, lancer l’algorithme de Sutherland-Hodgman et afficher le polygone découpé avec des couleurs différentes

---
+++ Unity/DecoupageRemplissage/Assets/Scripts/FenetrageManager.cs
@@ -0,0 +1,159 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class FenetrageManager : MonoBehaviour
+{
+    public Camera cameraScene;
+
+    public LineRenderer lrPolygone;
+    public LineRenderer lrFenetre;
+    public LineRenderer lrDecoupe;
+
+    public List<Point> pointsPolygone;
+    public List<Point> pointsFenetre;
+    public List<Point> pointsDecoupes;
+
+    public bool saisiePolygone = true;
+    public bool polygoneFerme = false;
+    public bool fenetreFermee = false;
+
+    private void Awake()
+    {
+        if(cameraScene == null)
+        {
+            cameraScene = Camera.main;
+        }
+
+        pointsPolygone = new List<Point>();
+        pointsFenetre = new List<Point>();
+        pointsDecoupes = new List<Point>();
+
+        InitialiserLineRenderers();
+    }
+
+    private void InitialiserLineRenderers()
+    {
+        if(lrPolygone == null)
+        {
+            GameObject goPolygone = new GameObject("LR_Polygone");
+            lrPolygone = goPolygone.AddComponent<LineRenderer>();
+        }
+
+        if(lrFenetre == null)
+        {
+            GameObject goFenetre = new GameObject("LR_Fenetre");
+            lrFenetre = goFenetre.AddComponent<LineRenderer>();
+        }
+
+        if(lrDecoupe == null)
+        {
+            GameObject goDecoupe = new GameObject("LR_Decoupe");
+            lrDecoupe = goDecoupe.AddComponent<LineRenderer>();
+        }
+
+        ConfigurerLineRenderer(lrPolygone, Color.green);
+        ConfigurerLineRenderer(lrFenetre, Color.blue);
+        ConfigurerLineRenderer(lrDecoupe, Color.red);
+    }
+
+    private void ConfigurerLineRenderer(LineRenderer lr, Color couleur)
+    {
+        lr.material = new Material(Shader.Find("Sprites/Default"));
+        lr.widthMultiplier = 0.05f;
+        lr.loop = false;
+        lr.positionCount = 0;
+        lr.startColor = couleur;
+        lr.endColor = couleur;
+    }
+
+    private void Update()
+    {
+        GérerChangementMode();
+        GérerSaisiePoints();
+        GérerValidationEtDecoupage();
+        MettreAJourLineRenderers();
+    }
+
+    private void GérerChangementMode()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            saisiePolygone = true;
+        }
+
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            saisiePolygone = false;
+        }
+    }
+
+    private void GérerSaisiePoints()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 pos = Input.mousePosition;
+            pos.z = Mathf.Abs(cameraScene.transform.position.z);
+            Vector3 monde = cameraScene.ScreenToWorldPoint(pos);
+
+            Point p = new Point(monde);
+
+            if(saisiePolygone && !polygoneFerme)
+            {
+                pointsPolygone.Add(p);
+            }
+            else if(!saisiePolygone && !fenetreFermee)
+            {
+                pointsFenetre.Add(p);
+            }
+        }
+    }
+
+    private void GérerValidationEtDecoupage()
+    {
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            if(saisiePolygone)
+            {
+                polygoneFerme = true;
+            }
+            else
+            {
+                fenetreFermee = true;
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(polygoneFerme && fenetreFermee && pointsPolygone.Count >= 3 && pointsFenetre.Count >= 3)
+            {
+                DecoupageSutherlandHodgman decoupage = new DecoupageSutherlandHodgman(pointsFenetre, pointsPolygone);
+                pointsDecoupes = decoupage.Decouper();
+            }
+        }
+    }
+
+    private void MettreAJourLineRenderers()
+    {
+        MettreAJourLineRenderer(lrPolygone, pointsPolygone, polygoneFerme);
+        MettreAJourLineRenderer(lrFenetre, pointsFenetre, fenetreFermee);
+        MettreAJourLineRenderer(lrDecoupe, pointsDecoupes, true);
+    }
+
+    private void MettreAJourLineRenderer(LineRenderer lr, List<Point> liste, bool fermer)
+    {
+        if(liste == null || liste.Count == 0)
+        {
+            lr.positionCount = 0;
+            lr.loop = false;
+            return;
+        }
+
+        lr.positionCount = liste.Count;
+        lr.loop = fermer && liste.Count >= 3;
+
+        for(int i = 0 ; i < liste.Count ; i++)
+        {
+            lr.SetPosition(i, liste[i].VersVector3());
+        }
+    }
+}
